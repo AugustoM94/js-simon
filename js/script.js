e23dfc40d2eaab048d3c4b240ac5ad953d80f8c0
AugustoM94 <email@github.com>
2023-10-20 18:46:04 +0200

    Project completed

--- js/script.js
+++ js/script.js
@@ -1,34 +1,60 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
 //inserire in array 5 numeri casuali da 1 a 100 e inserire in array
 function visualizzaNumeri() {
     let numeriCasuali = [];
     for (let i = 0; i < 5; i++) {
-        numeriCasuali.push(getRandomNumber(1, 100));  
+        numeriCasuali.push(getRandomNumber(1, 100));
     }
 
-
-    //indicato num visibili in dom
     let numeroContainer = document.getElementById('numero-container');
     numeroContainer.textContent = numeriCasuali.join(' ');
 
-    // timer 30 secondi
+    //timer 30 secondi
     setTimeout(function() {
         numeroContainer.textContent = '';
         abilitaInput(numeriCasuali);
     }, 30000); 
-
-    function abilitaInput(numeriCasuali) {
-        let inputContainer = document.getElementById('input-container');
-    
-        for (let i = 0; i < 5; i++) {
-            let input = document.createElement('input');
-            input.type = 'number';
-            input.placeholder = 'Inserisci il numero che ricordi';
-            inputContainer.appendChild(input);
-        }
-    }
 }
+
+function abilitaInput(numeriCasuali) {
+    let inputContainer = document.getElementById('input-container');
+    let confermaButton = document.getElementById('conferma');
+    let risultatoDiv = document.getElementById('risultato');
+
+ 
+
+
+//creare 5 input 
+    for (let i = 0; i < 5; i++) {
+        let input = document.createElement('input');
+        input.type = 'number';
+        input.placeholder = 'Inserisci un numero';
+        inputContainer.appendChild(input);
+    }
+
+    
+    confermaButton.addEventListener('click', function() {
+        let indovinati = [];
+        let inputFields = inputContainer.getElementsByTagName('input');
+        for (let i = 0; i < 5; i++) {
+            let numeroInserito = parseInt(inputFields[i].value, 10);
+            if (!isNaN(numeroInserito) && numeriCasuali.includes(numeroInserito)) {
+                indovinati.push(numeroInserito);
+            }
+        }
+
+        risultatoDiv.textContent = 'Hai indovinato ' + indovinati.length + ' numeri: ' + indovinati.join(', ');
+    });
+    
+    //Reset button per rimuovere il risultato e input e iniziare una nuova partita
+    const resetBtn = document.getElementById('reset');
+    resetBtn.addEventListener('click', function(){
+        inputContainer.innerHTML = '';
+        risultatoDiv.textContent = '';
+        visualizzaNumeri();
+    });
+}
+
+visualizzaNumeri();
